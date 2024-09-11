b4e512022cb7d0f3516ea331cc1a5dd44070a70c
Hanzala-Afridi <hanzalaafridi12345@gmail.com>
2024-09-12 00:43:31 +0500

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,24 @@
+// let a = 78
+// let b = "Hanzala";
+// c = 35.6
+// console.log(c);
+
+//Operator in javascript
+// In for example 3 + 4 '+' is the operator and 3,4 are operands
+// 1. unary operator - It has single operand for example (-, + )
+// 2. binary operator - It has two operand for example (x+5)
+//Operand - Entities on which operators operate 
+
+
+
+
+
+//Arithmetic  operator in actio in javascript
+let num1 = 67;
+let num2 = 55;
+console.log("This value of num1 + num2 is:" + (num1 + num2));
+console.log("This value of num1 + num2 is:" + (num1 - num2));
+console.log("This value of num1 + num2 is:" + (num1 * num2));
+console.log("This value of num1 + num2 is:" + (num1 / num2));
+console.log("This value of num1 + num2 is:" + (num1 ** num2));
+
