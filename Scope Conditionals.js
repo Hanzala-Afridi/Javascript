4a165647ba8f77b0c97254fc377eb97df5efa89e
Hanzala-Afridi <hanzalaafridi12345@gmail.com>
2024-09-16 22:14:53 +0500

    Add files via upload

---
+++ Scope Conditionals.js
@@ -0,0 +1,57 @@
+let string1 = "this is a string";
+console.log(string1);
+
+
+const a = "this connot be changed"
+console.log(a);
+
+
+
+
+let age = 2;
+if (age>18){
+    console.log("you can drink water");
+
+}
+else if(age==2){
+    console.log("age is 2");
+    
+}
+else{
+    console.log("you can drink cold drink");
+
+}
+const cups = 45;
+switch (cups) {
+    case 4:
+        console.log("the value of cups is 4");
+        
+        
+        break;
+
+   
+        case 5:
+            console.log("the value of cups is 5");
+            
+            
+            break;
+    
+        
+            case 6:
+                console.log("the value of cups is 6");
+                
+                
+                break;
+                case 9:
+                    console.log("the value of cups is 4");
+                    
+                    
+                    break;
+            
+               
+            default:
+                console.log("the value of cups none of 4,5,6,9");
+                
+                break;
+}
+
